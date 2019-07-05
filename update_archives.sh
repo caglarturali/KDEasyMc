4cd1f6b692ce5b07104062a55bd7d09e3256402c
Caglar Turali <caglarturali@gmail.com>
2019-07-05 15:42:23 +0300

    Add comments

--- update_archives.sh
+++ update_archives.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+###
+# Archive updater.
+# Run this script after making changes to files/dotfiles and/or files/systemwide dirs.
+##
+
 tar -C files/dotfiles -czvf files/dotfiles.tar.gz .
 tar -C files/systemwide -czvf files/systemwide.tar.gz .
